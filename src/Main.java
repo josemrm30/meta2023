aefa4ff00a3522079e7bc85fb39da272a67153a5
Jose <josemanuelrusmanjon@gmail.com>
2023-10-17 12:49:47 +0200

    fixed minor issues

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.logging.*;
 
 
@@ -19,42 +20,26 @@ public class Main {
             problems.add(problem);
         }
     }
-    public static void runAlgorithms() throws IOException {
+    public static void runAlgorithms() throws IOException, InterruptedException {
+        int num = config.getAlgorithms().size() * config.getFiles().size() * config.getSeeds().size();
+        CountDownLatch cdl = new CountDownLatch(num);
         for (int i = 0; i < config.getAlgorithms().size(); i++) {
             for (Problem problem : problems) {
-                try {
-                    CountDownLatch cdl = new CountDownLatch(config.getSeeds().size());
-                        for (int k = 0; k < config.getSeeds().size(); k++) {
-                            String logFile = "log/" + config.getAlgorithms().get(i) + "_" + problem.getName() + "_" + config.getSeeds().get(k) + ".txt";
-                            Metaheuristic meta = new Metaheuristic(problem, cdl, config.getSeeds().get(k), logFile, config.consoleLog, config.getIterations(), config.getAlgorithms().get(i));
-                            executor.execute(meta);
-                        }
-                        cdl.await();
-                    } catch (InterruptedException ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                for (int k = 0; k < config.getSeeds().size(); k++) {
+                    String logFile = "log/" + config.getAlgorithms().get(i) + "_" + problem.getName() + "_" + config.getSeeds().get(k) + ".txt";
+                    Metaheuristic meta = new Metaheuristic(problem, cdl, config.getSeeds().get(k), logFile, config.consoleLog, config.getIterations(), config.getAlgorithms().get(i));
+                    executor.execute(meta);
                 }
             }
         }
+        cdl.await();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         loadFiles(args);
 
 
-/*
-        LocalSearch Lsearch = new LocalSearch();
-        int size = config.getFiles().size();
-        int[][] flow = new int[size][size]; // Matriz de costos de flujo
-        int[][] loc = new int[size][size];  // Matriz de costos de asignación
-// Generar una solución inicial aleatoria
-        System.out.println("Local Search solution");
-        for (int i = 0; i < problems.size(); i++) {
-            int[] solActual = Lsearch.getInitialSolution(config.getFiles().size(), problems.get(i).getMatrix1(), problems.get(i).getMatrix2()).getSolutionList();
-            Lsearch.LocalSolution(problems.get(i).getMatrix1(), problems.get(i).getMatrix2(), problems.get(i).getMatrixSize(), config.getIterations());
-        }
-*/
-
         runAlgorithms();
-
+        executor.shutdown();
     }
 }
